95ab8a372b9398eaed86c3ee00be7d08b97f91d4
Shafqath <90248184+Shafqath00@users.noreply.github.com>
2024-11-25 23:12:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import bodyParser from "body-parser";
 import { console } from "inspector";
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT||3000;
 
 
 app.use(bodyParser.urlencoded({extended:true}));
